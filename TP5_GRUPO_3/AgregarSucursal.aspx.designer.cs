64e37a2098c90a407053b63c51b046d240977c4f
unknown <pedrodinguez2003@gmail.com>
2023-04-27 11:12:20 -0300

    subo mi primer aporte

--- TP5_GRUPO_3/AgregarSucursal.aspx.designer.cs
+++ TP5_GRUPO_3/AgregarSucursal.aspx.designer.cs
@@ -49,5 +49,14 @@ namespace TP5_GRUPO_3
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton lbtnEliminarSucursal;
+
+        /// <summary>
+        /// Control txtNombreSucursal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNombreSucursal;
     }
 }
