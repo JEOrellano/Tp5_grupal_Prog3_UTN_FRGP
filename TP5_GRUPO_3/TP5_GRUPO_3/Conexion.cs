c4c67b79895137e91b5f1b6a305539f34854a937
unknown <ignalecumberry2000@yahoo.com.ar>
2023-04-29 02:35:28 -0300

    Ejercicio 1 - Aporte 8

--- TP5_GRUPO_3/TP5_GRUPO_3/Conexion.cs
+++ TP5_GRUPO_3/TP5_GRUPO_3/Conexion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 
 namespace TP5_GRUPO_3
@@ -19,6 +20,22 @@ namespace TP5_GRUPO_3
             int filas = (int)comando.ExecuteNonQuery();/// INSERT-DELETE-UPDATE
             conexion.Close();
             return filas;
+        }
+  
+        public DropDownList cargaddl(DropDownList drop, string consulta) /// RECIBE UN DDL, LO CARGA Y LO RETORNA
+        {
+            SqlConnection conexion = new SqlConnection(ruta);
+            conexion.Open();
+
+            SqlCommand comando = new SqlCommand(consulta,conexion);
+            SqlDataReader dr = comando.ExecuteReader();
+            while (dr.Read())
+            {
+                drop.Items.Add(dr["DescripcionProvincia"].ToString());
+                drop.DataBind();
+            }
+            conexion.Close();
+            return drop;
         }
     }
 }
