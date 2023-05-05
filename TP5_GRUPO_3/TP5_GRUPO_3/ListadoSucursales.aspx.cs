c1645955eb88fe1349a77a4b4fe29e1b1b342605
Esteban <mobilelegendsbang2020bang@gmail.com>
2023-05-04 23:42:57 -0300

    Subo avance 18 Ejercicio 2 aporte 8

--- TP5_GRUPO_3/TP5_GRUPO_3/ListadoSucursales.aspx.cs
+++ TP5_GRUPO_3/TP5_GRUPO_3/ListadoSucursales.aspx.cs
@@ -14,8 +14,9 @@ namespace TP5_GRUPO_3
         {
             if(!IsPostBack)
             {
-                conexion.cargarGrid(grdDatos, "select Id_Sucursal,NombreSucursal,DescripcionSucursal,DescripcionProvincia from Sucursal " +
-                    "inner join Provincia on Sucursal.Id_ProvinciaSucursal = Provincia.Id_Provincia ");
+                string consulta = "SELECT S.Id_Sucursal, S.NombreSucursal AS Nombre, S.DescripcionSucursal AS Descripcion, P.DescripcionProvincia AS Provincia, S.DireccionSucursal AS Direccion" +
+                    " FROM Sucursal AS S INNER JOIN Provincia AS P ON S.Id_ProvinciaSucursal = P.Id_Provincia";                
+                conexion.cargarGrid(grdDatos, consulta);
             }
         }
 
@@ -32,7 +33,10 @@ namespace TP5_GRUPO_3
 
         protected void btnFiltrar_Click(object sender, EventArgs e)
         {
-
+            string consulta = "SELECT S.Id_Sucursal, S.NombreSucursal, S.DescripcionSucursal, P.DescripcionProvincia AS Provincia, S.DireccionSucursal" +
+                " FROM Sucursal AS S INNER JOIN Provincia AS P ON S.Id_ProvinciaSucursal = P.Id_Provincia" +
+                " WHERE S.Id_Sucursal = " + txtIdSucursal.Text;
+            conexion.cargarGrid(grdDatos,consulta);
         }
 
         protected void btnMostrarTodos_Click(object sender, EventArgs e)
